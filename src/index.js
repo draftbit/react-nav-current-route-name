6c2b192a360aaf919bf41b4f4f70a1b46d9d1679
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-12-05 15:25:09 -0600

    folder, init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { createSelector } from 'reselect'
+import { connect } from 'react-redux'
+
+const getNavRoutes = nav => nav.routes
+const getNavIndex = nav => nav.index
+
+export const getCurrentRouteName = createSelector(
+  [getNavRoutes, getNavIndex],
+  (routes, index) => {
+    const currentRouteIndex = routes[index].index
+    if (currentRouteIndex !== null || currentRouteIndex !== undefined) {
+      const currentRoute = routes[index].routes[currentRouteIndex]
+      return currentRoute.routeName
+    } else {
+      return null
+    }
+  }
+)
+
+export const getCurrentRoute = connect(state => {
+  const { nav } = state
+  return {
+    currentRoute: currentRoute({
+      index: nav.index,
+      routes: nav.routes
+    })
+  }
+})
