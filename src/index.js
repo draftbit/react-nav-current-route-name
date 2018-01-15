81df59706d4db8fba5854cdbb29327f79b9be72c
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2018-01-15 12:30:22 -0800

    check for undefined when logging out

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-import { createSelector } from 'reselect'
-import { connect } from 'react-redux'
+import { createSelector } from 'reselect';
+import { connect } from 'react-redux';
 
-const getNavRoutes = nav => nav.routes
-const getNavIndex = nav => nav.index
+const getNavRoutes = nav => nav.routes;
+const getNavIndex = nav => nav.index;
 
 export const getCurrentRouteName = createSelector(
   [getNavRoutes, getNavIndex],
   (routes, index) => {
-    const currentRouteIndex = routes[index].index
+    if (!Array.isArray(routes)) return null;
+    if (routes.length === 0) return null;
+
+    const currentRouteIndex = routes[index].index;
     if (currentRouteIndex !== null || currentRouteIndex !== undefined) {
-      const currentRoute = routes[index].routes[currentRouteIndex]
-      return currentRoute.routeName
+      const currentRoute = routes[index].routes[currentRouteIndex];
+      return currentRoute.routeName;
     } else {
-      return null
+      return null;
     }
   }
-)
+);
 
 export const withCurrentRouteName = connect(state => {
-  const { nav } = state
+  const { nav } = state;
   return {
     currentRoute: getCurrentRouteName({
       index: nav.index,
       routes: nav.routes
     })
-  }
-})
+  };
+});
